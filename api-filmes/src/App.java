5499784bac443c6fc469d3ed8cf40838ca8278f7
isahonorato <isabelladdotcom@gmail.com>
2023-03-31 21:57:21 -0300

    Refatoração

--- api-filmes/src/App.java
+++ api-filmes/src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -12,23 +7,21 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
         
         var geradora = new GeradorDeFigurinha();
-        for (Map<String,String> filme: listaDeFilmes) {
+        for (Map<String,String> Conteudo: listaDeConteudos) {
     
-                String urlImagem = filme.get("image");
-                String titulo = filme.get("title");
+                String urlImagem = Conteudo.get("image");
+                String titulo = Conteudo.get("title");
     
                 InputStream inputStream = new URL(urlImagem).openStream();
-                String nomeArquivo = titulo + ".png";
+                String nomeArquivo = "saida/" + titulo + ".png";
     
                 geradora.cria(inputStream, nomeArquivo);
     
