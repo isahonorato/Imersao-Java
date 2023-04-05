73a079f547103eab8ef93cb64f70c799ea699378
isahonorato <isabelladdotcom@gmail.com>
2023-04-05 00:06:05 -0300

    Order by ranking

--- linguagens-api/src/main/java/br/com/alura/Repository.java
+++ linguagens-api/src/main/java/br/com/alura/Repository.java
@@ -1,9 +1,11 @@
 package br.com.alura;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.Linguagem;
 
 public interface Repository extends MongoRepository<Linguagem, String> {
-    
+    List<Linguagem> findByOrderByRanking();
 }
--- linguagens-api/src/main/java/br/com/alura/linguagensapi/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagensapi/LinguagemController.java
@@ -21,7 +21,7 @@ public class LinguagemController {
 
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
-        List<Linguagem> linguagens = repositorio.findAll();
+        List<Linguagem> linguagens = repositorio.findByOrderByRanking();
         return linguagens;
     }
     @PostMapping("/linguagens")
